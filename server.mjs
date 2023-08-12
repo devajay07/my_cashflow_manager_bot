e49af3ca5311dfe8b7e5c5258795e8bd5d1427df
Ajay Yadav <ayyadav179@gmail.com>
2023-08-12 23:41:32 +0530

    try no 1000

--- server.mjs
+++ server.mjs
@@ -20,9 +20,11 @@ app.get("/", (req, res) => {
 
 app.post("/new-message", async (req, res) => {
   console.log("Incoming request body:", req.body);
-  const chatId = req.body.message.chat.id || req.body.my_chat_member.chat.id;
+  const chatId =
+    req.body.message?.chat?.id || req.body.my_chat_member?.chat?.id;
   const name =
-    req.body.message.chat.first_name || req.body.my_chat_member.from.first_name;
+    req.body.message?.chat?.first_name ||
+    req.body.my_chat_member?.from?.first_name;
   const text = req.body.message.text;
   let botResponse;
 
